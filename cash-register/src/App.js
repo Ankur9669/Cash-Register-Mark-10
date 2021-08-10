1af390f52c408d7c23b55025470855f0b0f534f7
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2021-08-10 17:46:11 +0530

    updated table structure

--- cash-register/src/App.js
+++ cash-register/src/App.js
@@ -8,58 +8,82 @@ function App()
   return (
     <div className="App">
       <div className = "container">
-        <h1>Cash Register Manager</h1>
-        <p>Enter the bill amount and cash given by the customer 
+        <h1 className = "app-heading">Cash Register Manager</h1>
+        <p className = "app-description">Enter the bill amount and cash given by the customer 
           and know minimum number of notes to return.
         </p>
 
         
         <div className = "bill-amount-container">
-          <label for = "bill-amount">Bill Amount</label>
-          <input type = "number"></input>
-          <button onClick = {() => setNextClicked(true)}>Next</button>
+          <label for = "bill-amount" className = "label">Bill Amount</label>
+          <input type = "number" id = "bill-amount" className = "input-bill-amount"></input>
+          <button onClick = {() => setNextClicked(true)} className = "button">Next</button>
         </div>
 
         {isNextClicked == true && 
         <div className = "amount-received-container">
-          <label for = "amount-received">Cash Given</label>
-          <input type = "number"></input>
-          <button onClick = {() => setSubmitClicked(true)}>Submit</button>
+          <label for = "amount-received" className = "label">Cash Given</label>
+          <input type = "number" id = "amount-received" className = "input-amount-received"></input>
+          <button onClick = {() => setSubmitClicked(true)} className = "button">Submit</button>
         </div>
         }
         
         {isSubmitClicked == true && 
         <div className = "notes-to-be-returned-container">
+           <label for = "notes-table" className = "label">Return Change</label>
           <table className = "notes-table">
-            <caption>Return Change</caption>
             <tbody>
             <tr>
               <th>No of notes</th>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td></td>
+              <td>Notes</td>
             </tr>
             <tr>
-              <th>Note</th>
-              <td>2000</td>
-              <td>500</td>
-              <td>200</td>
-              <td>100</td>
-              <td>50</td>
-              <td>20</td>
-              <td>10</td>
-              <td>5</td>
-              <td>1</td>
+               <th>2000</th>
+               <td>0</td>
             </tr>
-            </tbody>
-            
+            <tr>
+               <th>1000</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>500</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>200</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>100</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>50</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>20</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>10</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>5</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>2</th>
+               <td>0</td>
+            </tr>
+            <tr>
+               <th>1</th>
+               <td>0</td>
+            </tr>
+            </tbody> 
           </table>
+          <h1>Total Required Notes Are:</h1>
         </div>
         }
       </div>
